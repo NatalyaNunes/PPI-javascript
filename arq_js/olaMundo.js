ee0fed3ae360130580534d482d68d0b682ee7a26
Natálya Gabriele Nunes de Azevedo <nunes.natalya@academico.ifrn.edu.br>
2023-06-26 10:14:52 -0300

    primeiro commit

---
+++ arq_js/olaMundo.js
@@ -0,0 +1,9 @@
+function portugues(){
+    alert("Olá Mundo!");
+}
+
+document.getElementById("portugues").addEventListener("click", portugues)
+
+document.getElementById("alemao").onclick = function (){
+    alert("Hallo Wereld!");
+}
