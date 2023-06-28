99d606b6864876a8682430e6544e63b2bf3c63a5
Natálya Gabriele Nunes de Azevedo <nunes.natalya@academico.ifrn.edu.br>
2023-06-28 10:58:30 -0300

    atualizacao

--- arq_js/olaMundo.js
+++ arq_js/olaMundo.js
@@ -2,8 +2,10 @@ function portugues(){
     alert("Olá Mundo!");
 }
 
-document.getElementById("portugues").addEventListener("click", portugues)
+document.getElementById("portugues").addEventListener("click", portugues);
 
 document.getElementById("alemao").onclick = function (){
     alert("Hallo Wereld!");
-}
+};
+
+console.log(document);
